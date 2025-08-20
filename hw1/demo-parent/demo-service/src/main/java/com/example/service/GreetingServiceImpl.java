2890eda5e571f5dcf3839a69e0f3e23953515a55
Zack Wang <wzack9678@gmail.com>
2025-08-20 17:10:13 -0400

    Sec 2.4.6

--- hw1/demo-parent/demo-service/src/main/java/com/example/service/GreetingServiceImpl.java
+++ hw1/demo-parent/demo-service/src/main/java/com/example/service/GreetingServiceImpl.java
@@ -4,7 +4,7 @@ import com.example.api.GreetingService;
 public class GreetingServiceImpl implements GreetingService {
     @Override
     public void greet(String msg) {
-        System.out.println("hello world"); // 按题意固定打印
+        System.out.println("hello world pd"); // 按题意固定打印
     }
     public static void main(String[] args) {
         new GreetingServiceImpl().greet("");
