f6635990ec3239c5bedbdee9fc5efdcb27e5f1da
j 宋 <jsou@js-MacBook-Pro.attlocal.net>
2025-08-20 23:46:42 -0500

    hw

---
+++ hw1/demo-parent/demo-service/src/main/java/org/example/GreetingServiceImpl.java
@@ -0,0 +1,14 @@
+package org.example;
+import org.example.GreetingService;
+
+public class GreetingServiceImpl implements GreetingService{
+    @Override
+    public void greet(String msg) {
+        System.out.println("hello world" + msg);
+    }
+
+    public static void main(String[] args) {
+        GreetingService service = new GreetingServiceImpl();
+        service.greet(" :)");
+    }
+}
--- hw1/demo/src/main/java/org/example/StringUtilsDemo.java
+++ hw1/demo/src/main/java/org/example/StringUtilsDemo.java
@@ -1,2 +1,12 @@
-package org.example;public class StringUtilsDemo {
+package org.example;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class StringUtilsDemo {
+    public static boolean isEmptyString(String input) {
+        return StringUtils.isEmpty(input);
+    }
 }
+
+
+
